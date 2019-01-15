10cf94147d418bbe7d89d47795b4eac74fbdd042
alexandraback <alexandra1.back@gmail.com>
2019-01-15 15:09:08 +0100

    FO-1847: Mock av hent registrering

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,43 @@
 import * as React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {fetchData} from "./utils/fetchData";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+interface SisteArbeidsforhold {
+    sisteStilling : {
+        label: string,
+        konseptId: number,
+        styrk08: string,
+    },
+}
+
+class App extends React.Component<{}, SisteArbeidsforhold> {
+
+    constructor(props: {}) {
+        super(props);
+        this.state = { sisteStilling: {
+            label: "",
+            konseptId: 0,
+            styrk08: "",
+            }};
+    }
+
+
+    componentDidMount() {
+        fetchData<SisteArbeidsforhold>("/veilarbregistrering/api/registrering").then(
+            data => {
+                this.setState(data);
+            }
+        )
+
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {this.state.sisteStilling.label}
+            </div>
+        );
+    }
 }
 
 export default App;
