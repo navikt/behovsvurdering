f11f61932eda472f9650109b63a7099caa39f16a
alexandraback <alexandra1.back@gmail.com>
2019-01-17 10:31:37 +0100

    FO-1847: banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import StillingInfo from "./StillingInfo";
 import AppProviders from "./context/AppProvider";
+import Banner from "./Banner";
 
 
 
@@ -10,6 +11,7 @@ class App extends React.Component {
         return (
             <AppProviders>
                 <div className="wrapper">
+                    <Banner/>
                     <StillingInfo/>
                 </div>
             </AppProviders>
