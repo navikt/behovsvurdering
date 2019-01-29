be99d5a08394440adfaedd62b78d18bbec6f5f07
Izis Frøyhov <izis.froyhov@nav.no>
2019-01-29 12:12:02 +0100

    FO-1869: Gjøre banner lik som registrering

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import {Sidetittel} from 'nav-frontend-typografi';
+import {Systemtittel} from 'nav-frontend-typografi';
 
 function Banner () {
     return(
-        <div className="banner">
+        <div className="bs-banner">
             <div className="banner__innhold">
-                <Sidetittel>Dine jobbmuligheter</Sidetittel>
+                <Systemtittel tag="h1">Dine jobbmuligheter</Systemtittel>
             </div>
         </div>
     )
