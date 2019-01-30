75f78fe0e90f86e534d9c99fa93a346583803a3e
Izis Frøyhov <izis.froyhov@nav.no>
2019-01-30 14:35:52 +0100

    FO-1852: legge på credentials

--- src/api/api.ts
+++ src/api/api.ts
@@ -13,23 +13,41 @@ export interface OppfolgingStatusType {
     underOppfolging: boolean;
 }
 
+function getCookie(name: string) {
+    const re = new RegExp(`${name}=([^;]+)`);
+    const match = re.exec(document.cookie);
+    return match !== null ? match[1] : '';
+}
+
+function getHeaders() {
+    return new Headers({
+        'Content-Type': 'application/json',
+        'NAV_CSRF_PROTECTION': getCookie('NAV_CSRF_PROTECTION'), // eslint-disable-line quote-props
+    });
+}
+
+const CONFIG = {
+    credentials: ('same-origin' as RequestCredentials),
+    headers: getHeaders(),
+};
+
 export function hentSisteStilling(errorHandler:(response?: Response) => Promise<SisteStillingType>): Promise<SisteStillingType> {
-    return fetchData<RegistreringDataType>(API_VEILARBREGISTRERING)
+    return fetchData<RegistreringDataType>(API_VEILARBREGISTRERING, CONFIG)
         .then((registeringsData: RegistreringDataType) => ({sisteStilling: registeringsData.registrering.sisteStilling}));
 }
 
 export function hentOppfolgingStatus():Promise<OppfolgingStatusType> {
-    return fetchData<OppfolgingStatusType>(API_VEILARBOPPFOLGING).then((oppfolgingStatus: OppfolgingStatusType) => ({underOppfolging: oppfolgingStatus.underOppfolging}));
+    return fetchData<OppfolgingStatusType>(API_VEILARBOPPFOLGING, CONFIG).then((oppfolgingStatus: OppfolgingStatusType) => ({underOppfolging: oppfolgingStatus.underOppfolging}));
 }
 
 export function hentKommuneOgStillinger([kommunnenummer,styrkkode]: string[], errorHandler:(response?: Response) => Promise<KommuneOgLedigeStillinger>): Promise<KommuneOgLedigeStillinger>{
-    return fetchData<KommuneOgLedigeStillinger>(`${API_MIA}?kommunennummer=${kommunnenummer}&styrkkode=${styrkkode}`, {}, errorHandler)
+    return fetchData<KommuneOgLedigeStillinger>(`${API_MIA}?kommunennummer=${kommunnenummer}&styrkkode=${styrkkode}`, CONFIG, errorHandler)
 }
 
 export function hentGeografiskTilknytning(): Promise<GeografiskTilknytning> {
-    return fetchData<GeografiskTilknytning>(API_VEILARBPERSON);
+    return fetchData<GeografiskTilknytning>(API_VEILARBPERSON, CONFIG);
 }
 
 export function postDialog(data: any): Promise<void> {
-    return fetchData<void>(API_VEILARBDIALOG, {method: 'post', body: JSON.stringify(data)});
+    return fetchData<void>(API_VEILARBDIALOG, {method: 'post', body: JSON.stringify(data), ...CONFIG});
 }
