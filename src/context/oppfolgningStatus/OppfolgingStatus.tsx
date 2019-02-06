d1159a5d7d48e310ef1a0b57daa36854ba1ad017
dervism <dervis4@gmail.com>
2019-02-06 17:44:47 +0100

    KAIZEN-0: Lager en advarsel info panel dersom en bruker ikke er under oppfølgning.

--- src/context/oppfolgningStatus/OppfolgingStatus.tsx
+++ src/context/oppfolgningStatus/OppfolgingStatus.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { hentOppfolgingStatus, OppfolgingStatusType } from '../../api/api';
 import DataFetcher from '../../utils/dataFetcher';
+import AlertStripe from 'nav-frontend-alertstriper';
 
 interface OppfolgingStatusProps {
     children: React.ReactNode;
@@ -10,8 +11,14 @@ function OppfolgingStatus(props: OppfolgingStatusProps) {
     return (
         <DataFetcher<OppfolgingStatusType> fetchFunc={hentOppfolgingStatus}>
             {(data: OppfolgingStatusType) => {
-                if (!data.underOppfolging) {
-                    return <div/>;
+                if (data.underOppfolging) {
+                    return (
+                        <div id="ikke-under-oppfolgning-container">
+                            <AlertStripe type="info" className="ikke-under-oppfolgning-boks">
+                                Du er ikke under arbeidsrettet oppfølgning.
+                            </AlertStripe>
+                        </div>
+                    );
                 }
                 return props.children;
             }}
