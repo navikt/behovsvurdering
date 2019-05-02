c32829485756bce416a997bb3783cd3a3406b80f
hakonph <hakon.planke.holm@nav.no>
2019-05-02 09:36:45 +0200

    [FO-2009] fjerner export

--- src/ScrolTopOnChange.ts
+++ src/ScrolTopOnChange.ts
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 
-export function usePrevious(value: Object): Object | undefined {
+function usePrevious(value: Object): Object | undefined {
     const ref = useRef<Object>();
     useEffect(() => {
         ref.current = value;
@@ -8,13 +8,13 @@ export function usePrevious(value: Object): Object | undefined {
     return ref.current;
 }
 
-export function useHasCangedChange(value: Object): boolean {
+function useHasCangedChange(value: Object): boolean {
     let previous = usePrevious(value);
     return previous !== value;
 }
 
 export function useScrollTopOnChange(value: Object) {
-    let changed = useHasCangedChange(value);
+    const changed = useHasCangedChange(value);
 
     if (changed) {
         window.scroll(0, 0);
