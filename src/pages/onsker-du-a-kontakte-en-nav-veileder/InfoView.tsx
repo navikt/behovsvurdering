658fb2471a2c5e7ba5efc5a155fc26e467e848e3
Alexander Gård <alexander.gard@nav.no>
2019-08-15 14:14:08 +0200

    FO-2290 Tilpasset styling på hovedside til desktop

--- src/pages/onsker-du-a-kontakte-en-nav-veileder/InfoView.tsx
+++ src/pages/onsker-du-a-kontakte-en-nav-veileder/InfoView.tsx
@@ -1,49 +1,46 @@
 import React from 'react';
-import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
+import { Normaltekst, Systemtittel } from 'nav-frontend-typografi';
 import jobbSokingSVG from './img/jobbsokertips.svg';
 import cvSVG from './img/cv.svg';
 import sokSVG from './img/stillingsok.svg';
 import mugSVG from './img/design-mug.svg';
 import dialogSVG from './img/dialog.svg';
 import ScrollKnapp from '../../components/scroll-knapp/ScrollKnapp';
-import './index.less';
+import './InfoView.less';
 
-function InfoView() {
-
-    return (
-        <div className="beholder gray">
-            <div className="rad">
-                <Undertittel className="rad-item">
-                    På nav.no får du:
-                </Undertittel>
-                <div className="rad-item">
-                    <div className="liste-item">
-                        <img src={jobbSokingSVG} className="ikon" alt=""/>
-                        <Normaltekst>Jobbsøkertips som passer din situasjon</Normaltekst>
-                    </div>
-                    <div className="liste-item">
-                        <img src={cvSVG} className="ikon" alt=""/>
-                        <Normaltekst>En CV som gjør deg synlig for arbeidsgivere som ser etter din kompetanse.</Normaltekst>
-                    </div>
-                    <div className="liste-item">
-                        <img src={sokSVG} className="ikon" alt=""/>
-                        <Normaltekst>Oversikt over ledige stillinger i hele landet.</Normaltekst>
-                    </div>
-                    <div className="liste-item">
-                        <img src={mugSVG} className="ikon" alt=""/>
-                        <Normaltekst>En plan for å holde oversikt over jobbsøkingen.</Normaltekst>
-                    </div>
-                    <div className="liste-item padding-bottom">
-                        <img src={dialogSVG} className="ikon" alt=""/>
-                        <Normaltekst>En digital dialog hvor du kan stille veilederen din spørsmål.</Normaltekst>
-                    </div>
-                </div>
-            </div>
-            <div className="scroll-knapp-container">
+const InfoView = () => (
+    <section className="info-view gray">
+        <Systemtittel className="info-view__header">
+            På nav.no får du:
+        </Systemtittel>
+        <ul className="info-view__item-list">
+            <InfoItem ikon={jobbSokingSVG}>
+                Jobbsøkertips som passer din situasjon.
+            </InfoItem>
+            <InfoItem ikon={cvSVG}>
+                En CV som gjør deg synlig for arbeidsgivere som ser etter din kompetanse.
+            </InfoItem>
+            <InfoItem ikon={sokSVG}>
+                Oversikt over ledige stillinger i hele landet.
+            </InfoItem>
+            <InfoItem ikon={mugSVG}>
+                En plan for å holde oversikt over jobbsøkingen.
+            </InfoItem>
+            <InfoItem ikon={dialogSVG}>
+                En digital dialog hvor du kan stille veilederen din spørsmål.
+            </InfoItem>
+        </ul>
+        <div className="scroll-knapp-container">
             <ScrollKnapp className="middle-scroll"/>
-            </div>
         </div>
-    );
-}
+    </section>
+);
+
+const InfoItem = (props: { ikon: string, children: React.ReactNode }) => (
+    <li className="info-view__item">
+        <img src={props.ikon} className="info-view__item--ikon" alt=""/>
+        <Normaltekst>{props.children}</Normaltekst>
+    </li>
+);
 
 export default InfoView;
--- src/pages/onsker-du-a-kontakte-en-nav-veileder/InputView.tsx
+++ src/pages/onsker-du-a-kontakte-en-nav-veileder/InputView.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Systemtittel } from 'nav-frontend-typografi';
 import { RadioPanelGruppe } from 'nav-frontend-skjema';
 import { Hovedknapp } from 'nav-frontend-knapper';
+import './InputView.less';
 
 export const NEI = 'Nei';
 export const KANSKJE = 'Kanskje senere';
@@ -23,43 +24,40 @@ function InputView(props: InputViewProps) {
 
     const feil = feilState ? {feilmelding: 'Obligatorisk felt'} : undefined;
     return (
-        <div className="beholder">
-            <div className="rad">
-                <Systemtittel className="rad-item center-text">
-                    {SPORSMAL}
-                </Systemtittel>
-                <RadioPanelGruppe
-                    className="rad-item"
-                    legend=""
-                    name=""
-                    radios={[
-                        {label: NEI, disabled: props.disabled, value: NEI},
-                        {label: KANSKJE, disabled: props.disabled, value: KANSKJE},
-                        {label: JA, disabled: props.disabled, value: JA},
-                        {label: USIKKER, disabled: props.disabled, value: USIKKER},
-                    ]}
-                    checked={value}
-                    onChange={(_, val) => setValue(val)}
-                    feil={feil}
-                />
-                <div className="center">
-                    <Hovedknapp
-                        spinner={props.disabled}
-                        disabled={props.disabled}
-                        onClick={() => {
-                            if (value === undefined) {
-                                setFeil(true);
-                            } else {
-                                setFeil(false);
-                                props.onSubmit(value);
-                            }
-                        }}
-                    >
-                        Send
-                    </Hovedknapp>
-                </div>
-            </div>
-        </div>
+        <section className="input-view">
+            <Systemtittel className="rad-item center-text">
+                {SPORSMAL}
+            </Systemtittel>
+            <RadioPanelGruppe
+                className="input-view__radio-gruppe"
+                legend=""
+                name=""
+                radios={[
+                    {label: NEI, disabled: props.disabled, value: NEI},
+                    {label: KANSKJE, disabled: props.disabled, value: KANSKJE},
+                    {label: JA, disabled: props.disabled, value: JA},
+                    {label: USIKKER, disabled: props.disabled, value: USIKKER},
+                ]}
+                checked={value}
+                onChange={(_, val) => setValue(val)}
+                feil={feil}
+            />
+            <Hovedknapp
+                className="input-view__send-knapp"
+                spinner={props.disabled}
+                disabled={props.disabled}
+                onClick={() => {
+                    if (value === undefined) {
+                        setFeil(true);
+                    } else {
+                        setFeil(false);
+                        props.onSubmit(value);
+                    }
+                }}
+            >
+                Send
+            </Hovedknapp>
+        </section>
     );
 }
 
