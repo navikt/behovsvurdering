e6535327f70bd28bb2ac45691b17d44c43a9ad61
Alexander Gård <alex.gaard@hotmail.com>
2020-10-01 16:24:05 +0200

    Fjern login spinner

--- src/index.tsx
+++ src/index.tsx
@@ -2,27 +2,11 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import 'babel-polyfill';
 import 'whatwg-fetch';
-import { Auth, EnforceLoginLoader } from '@navikt/nav-dekoratoren-moduler/dist';
 import './index.less';
 import App from './App';
-import { useState } from 'react';
-
-const AppWithLogin = () => {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const authCallback = (auth: Auth) => {
-        setIsAuthenticated(auth.authenticated);
-    };
-
-    return (
-        <EnforceLoginLoader authCallback={authCallback}>
-            {isAuthenticated ?  <App /> : <></>}
-        </EnforceLoginLoader>
-    );
-};
 
 if (process.env.REACT_APP_MOCK === 'true') {
     require('./mock');
-    ReactDOM.render(<App />, document.getElementById('root'));
-} else {
-    ReactDOM.render(<AppWithLogin />, document.getElementById('root'));
 }
+
+ReactDOM.render(<App />, document.getElementById('root'));
