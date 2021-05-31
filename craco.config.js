4613c5bfbd37727695e9417c02e87be81407d14e
Alexander Gård <alex.gaard@hotmail.com>
2021-05-31 08:10:29 +0200

    Forenklet craco config

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,5 @@
-const CracoLessPlugin = require('craco-less');
-const NpmImportPlugin = require('less-plugin-npm-import');
-
 module.exports = {
     plugins: [
-        { plugin: CracoLessPlugin,
-            options: {
-                lessLoaderOptions: {
-                    loader: new NpmImportPlugin({ prefix: '~' })
-                }
-            }
-        },
+        { plugin: require('craco-less') }
     ]
 };
