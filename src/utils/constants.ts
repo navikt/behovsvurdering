c1d24fde06992ff27f561e33c372cac18eccd849
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-10-04 12:20:15 +0200

    r legger til konstant for DITT_NAV_URL

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,5 @@
+import { erProduksjon } from './er-produksjon';
 
 export const APP_NAME = 'behovsvurdering';
+
+export const DITT_NAV_URL = erProduksjon() ? 'https://www.nav.no/person/dittnav' : 'https://www.dev.nav.no/person/dittnav';
